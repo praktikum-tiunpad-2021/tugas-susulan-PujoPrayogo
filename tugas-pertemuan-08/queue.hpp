a0680228163ab83b034af2b797f3d5e0ac02c512
PujoPrayogo <77614139+PujoPrayogo@users.noreply.github.com>
2021-06-06 19:06:59 +0700

    Update queue.hpp

--- tugas-pertemuan-08/queue.hpp
+++ tugas-pertemuan-08/queue.hpp
@@ -10,6 +10,9 @@ namespace priority_queue {
 template <typename T>
 struct Element {
   // Implementasikan di sini.
+  T value;
+  T priority;
+  Element* next;
 };
 
 template <typename T>
@@ -21,6 +24,8 @@ using ElementPtr = Element<T> *;
 template <typename T>
 struct Queue {
   // Implementasikan di sini.
+  ElementPtr<T> Head;
+  ElementPtr<T> Tail;
 };
 
 /**
@@ -31,6 +36,11 @@ struct Queue {
 template <typename T>
 Queue<T> new_queue() {
   // Implementasikan di sini.
+  Queue<T> q;
+  q.Head = nullptr;
+  q.Tail = nullptr;
+  return q;
+  
 }
 
 /**
@@ -43,6 +53,36 @@ Queue<T> new_queue() {
 template <typename T>
 void enqueue(Queue<T> &q, const T &value, int priority) {
   // Implementasikan di sini.
+  ElementPtr<T> newElement;
+  newElement = new Element<T>;
+  newElement->value = value;
+  newElement->priority = priority;
+  newElement->next = nullptr;
+
+  ElementPtr<T> pRev = nullptr;
+  ElementPtr<T> pHelp = q.Head;
+
+  if(q.Head == nullptr){
+    q.Head = newElement;
+    q.Tail = newElement;
+  }else{
+    while(newElement->priority <= pHelp->priority){
+      if(pHelp->next == nullptr){
+        break;
+      }
+      pRev = pHelp;
+      pHelp = pHelp->next;
+    }if(pHelp == q.Head && newElement->priority > pHelp->priority ){
+        newElement->next = q.Head;
+        q.Head = newElement;
+    }else if(pHelp == q.Tail && newElement->priority <= pHelp->priority){
+        pHelp->next = newElement;
+        q.Tail = newElement;
+      }else{
+        pRev->next = newElement;
+        newElement->next = pHelp;
+    }
+  } 
 }
 
 /**
@@ -54,6 +94,7 @@ void enqueue(Queue<T> &q, const T &value, int priority) {
 template <typename T>
 T top(const Queue<T> &q) {
   // Implementasikan di sini.
+  return q.Head->value;
 }
 
 /**
@@ -64,6 +105,23 @@ T top(const Queue<T> &q) {
 template <typename T>
 void dequeue(Queue<T> &q) {
   // Implementasikan di sini.
+  ElementPtr<T> delElement;
+  if(q.Head == nullptr)
+  {
+    delElement = nullptr;
+  }
+  else if(q.Head->next == nullptr)
+  {
+    delElement = q.Head;
+    q.Head = nullptr;
+    q.Tail = nullptr;
+  }
+  else
+  {
+    delElement = q.Head;
+    q.Head = q.Head->next;
+    delElement->next = nullptr;
+  }
 }
 
 }  // namespace priority_queue
